c14f2ea038de50d406f8b5254353a0a8a23e7b42
soc day <socpro13579@gmail.com>
2025-05-12 13:48:57 +0700

    Upload Bai Cho Mon PRN222

---
+++ Models/Lab1PharmaceuticalDbContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PharmacyConsoleApp.Models;
+
+public partial class Lab1PharmaceuticalDbContext : DbContext
+{
+    public Lab1PharmaceuticalDbContext()
+    {
+    }
+
+    public Lab1PharmaceuticalDbContext(DbContextOptions<Lab1PharmaceuticalDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Manufacturer> Manufacturers { get; set; }
+
+    public virtual DbSet<MedicineInformation> MedicineInformations { get; set; }
+
+    public virtual DbSet<StoreAccount> StoreAccounts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Lab1PharmaceuticalDB;User Id=sa;Password=8/zC-0E1@jA;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Manufacturer>(entity =>
+        {
+            entity.HasKey(e => e.ManufacturerId).HasName("PK__Manufact__357E5CA15F2A5178");
+
+            entity.ToTable("Manufacturer");
+
+            entity.Property(e => e.ManufacturerId)
+                .HasMaxLength(20)
+                .HasColumnName("ManufacturerID");
+            entity.Property(e => e.ContactInformation).HasMaxLength(120);
+            entity.Property(e => e.CountryofOrigin).HasMaxLength(250);
+            entity.Property(e => e.ManufacturerName).HasMaxLength(100);
+            entity.Property(e => e.ShortDescription).HasMaxLength(400);
+        });
+
+        modelBuilder.Entity<MedicineInformation>(entity =>
+        {
+            entity.HasKey(e => e.MedicineId).HasName("PK__Medicine__4F2128F08DC1FD73");
+
+            entity.ToTable("MedicineInformation");
+
+            entity.Property(e => e.MedicineId)
+                .HasMaxLength(30)
+                .HasColumnName("MedicineID");
+            entity.Property(e => e.ActiveIngredients).HasMaxLength(200);
+            entity.Property(e => e.DosageForm).HasMaxLength(400);
+            entity.Property(e => e.ExpirationDate).HasMaxLength(120);
+            entity.Property(e => e.ManufacturerId)
+                .HasMaxLength(20)
+                .HasColumnName("ManufacturerID");
+            entity.Property(e => e.MedicineName).HasMaxLength(160);
+            entity.Property(e => e.WarningsAndPrecautions).HasMaxLength(400);
+
+            entity.HasOne(d => d.Manufacturer).WithMany(p => p.MedicineInformations)
+                .HasForeignKey(d => d.ManufacturerId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__MedicineI__Manuf__3C69FB99");
+        });
+
+        modelBuilder.Entity<StoreAccount>(entity =>
+        {
+            entity.HasKey(e => e.StoreAccountId).HasName("PK__StoreAcc__42D52A6A51133646");
+
+            entity.ToTable("StoreAccount");
+
+            entity.HasIndex(e => e.EmailAddress, "UQ__StoreAcc__49A14740C7DE632D").IsUnique();
+
+            entity.Property(e => e.StoreAccountId)
+                .ValueGeneratedNever()
+                .HasColumnName("StoreAccountID");
+            entity.Property(e => e.EmailAddress).HasMaxLength(90);
+            entity.Property(e => e.StoreAccountDescription).HasMaxLength(140);
+            entity.Property(e => e.StoreAccountPassword).HasMaxLength(90);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/MedicineInformation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PharmacyConsoleApp.Models;
+
+public partial class MedicineInformation
+{
+    public string MedicineId { get; set; } = null!;
+
+    public string MedicineName { get; set; } = null!;
+
+    public string ActiveIngredients { get; set; } = null!;
+
+    public string? ExpirationDate { get; set; }
+
+    public string DosageForm { get; set; } = null!;
+
+    public string WarningsAndPrecautions { get; set; } = null!;
+
+    public string? ManufacturerId { get; set; }
+
+    public virtual Manufacturer? Manufacturer { get; set; }
+}
